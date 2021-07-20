9f788bbeddea0aa3b62ae801b2f325249f2a10cf
Thomas <GroovyHooked@dafrenchie2002@yahoo.fr>
2021-07-20 23:43:51 +0200

    AJAX request with jQuery

--- main.js
+++ main.js
@@ -1,45 +1,22 @@
-const request = new XMLHttpRequest();
-
-request.onreadystatechange = function (event){
-    if (this.readyState === 4){
-        if(this.status === 200){
-            //console.log("Réponse reçue: %s", this.responseText)
-            var goodObject = parseJson(JSON.parse(this.responseText)[0]);
-            console.log(goodObject)
-        } else {
-            console.log("Status de la réponse: %s", this.status, this.responseText);
-        }
-    }
+var form = $('#userForm');
+var url = 'http://localhost/TutoAjax/123user.json';
+var errorSpan = $('#error');
+var successCallBack = function (response){
+    console.log(response);
 }
-
-function parseJson(object)  {
-    for (var key in object) {
-        if(object.hasOwnProperty(key)){
-            var value = object[key]
-            switch (true) {
-                case value === "true" || value === "false":
-                    object[key] = value === "true";
-                    break;
-                case /\d/.test(value):
-                    object[key] = parseInt(value);
-                    break;
-            }
-        }
-        for(var underKey in value){
-            //console.log('test');
-            if(value.hasOwnProperty(underKey)){
-
-                var newValue = value[underKey];
-                //console.log(newValue);
-                switch (true) {
-                    case /\d/.test(newValue):
-                        value[underKey] = parseInt(newValue);
-                        break;
-                }
-            }
-        }
-    }
-    return object;
+var errorCallBack = function (response){
+    console.log(response);
+    errorSpan.append(response.responseText);
 }
-request.open('GET', 'http://localhost/TutoAjax/user.json', true);
-request.send(null);
+var getOptions ={
+    method: 'POST',
+    async: true,
+    cache: false,
+    success: successCallBack,
+    error: errorCallBack
+}
+form.on('submit', function (e){
+    e.preventDefault();
+    getOptions.data = $(this).serialize();
+    $.ajax(url, getOptions);
+})
